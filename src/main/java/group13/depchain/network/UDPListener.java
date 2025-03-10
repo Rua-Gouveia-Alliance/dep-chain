4535ea88a95a361d439e7a2209d324fccc490937
Gonçalo Rua <goncalonrua@tecnico.ulisboa.pt>
2025-03-10 04:08:34 +0000

    implement network methods

--- src/main/java/group13/depchain/network/UDPListener.java
+++ src/main/java/group13/depchain/network/UDPListener.java
@@ -1,3 +1,27 @@
 package group13.depchain.network;
 
-public class UDPListener {}
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UDPListener {
+    private DatagramSocket socket;
+
+    public UDPListener(int port) throws SocketException {
+        this.socket = new DatagramSocket(port);
+    }
+
+    public String receive() throws IOException {
+        byte[] received = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(null, received.length);
+
+        socket.receive(packet);
+        return new String(packet.getData());
+    }
+
+    public void close() {
+        if (socket != null && !socket.isClosed())
+            socket.close();
+    }
+}
